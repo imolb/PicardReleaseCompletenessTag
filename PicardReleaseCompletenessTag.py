b47f7f8891ad7763efd0621c82587e7778b62a5d
imolb <imolb@web.de>
2021-05-14 17:25:05 +0200

    Changed plugin name. Aligned function names. Fixed typo.

--- PicardReleaseCompletenessTag.py
+++ PicardReleaseCompletenessTag.py
@@ -1,7 +1,7 @@
-PLUGIN_NAME = "PicardReleaseCompletenessTag"
+PLUGIN_NAME = "Release Completeness Tag"
 PLUGIN_AUTHOR = "imolb"
 PLUGIN_DESCRIPTION = """
-Set a user defined tag TXXX:releasecompleteness if an release is complete i.e. 
+Set a user defined tag TXXX:releasecompleteness if a release is complete i.e. 
 all tracks of the release exists.
 This is useful to filter in the playback app for only complete releases
 (requires an app capable of filtering by user defined tags).
@@ -16,7 +16,7 @@ USER_DEFINED_TAG_NAME = "releasecompleteness"
 from picard.file import register_file_post_addition_to_track_processor, register_file_post_removal_from_track_processor
 
 
-def set_complete_release_tag(track, file_input):
+def set_release_completeness_tag(track, file_input):
     album = track.album
     for file in album.iterfiles():
         if album.is_complete():
@@ -25,5 +25,5 @@ def set_complete_release_tag(track, file_input):
             file.metadata[USER_DEFINED_TAG_NAME] = "not complete"
 
 
-register_file_post_addition_to_track_processor(set_complete_release_tag)
-register_file_post_removal_from_track_processor(set_complete_release_tag)
+register_file_post_addition_to_track_processor(set_release_completeness_tag)
+register_file_post_removal_from_track_processor(set_release_completeness_tag)
